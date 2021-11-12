d9f33a6bc2c84ea8c20c50bfe58e81ab44596b90
nihkilagarwal204 <anikhil428@gmail.com>
2021-11-13 00:13:44 +0530

    first commit

---
+++ server/database/image/index.js
@@ -0,0 +1,7 @@
+import mongoose from "mongoose";
+
+const ImageSchema = new mongoose.Schema({
+    images: [{location: {type: String, required: true}}]
+});
+
+export const ImageModel = mongoose.model("Images", ImageSchema);
---
+++ server/database/menu/index.js
@@ -0,0 +1,8 @@
+import mongoose from "mongoose";
+
+const MenuSchema = new mongoose.Schema({
+  menus: [{ name: { type: String, required: true }, items: [{type: mongoose.Types.ObjectId, ref: "Foods"}] }],
+  recomended: [{type: mongoose.Types.ObjectId, ref: "Foods", unique: true}]
+});
+
+export const MenuModel = mongoose.model("Menu", MenuSchema);
