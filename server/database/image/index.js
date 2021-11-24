d23bcf9b15df67d05d66cb521e5dda9eb5252198
NIKHIL AGARWAL <45812764+nikhilagarwal204@users.noreply.github.com>
2021-11-24 10:18:50 +0530

    Delete server directory

--- server/database/image/index.js
+++
@@ -1,7 +0,0 @@
-import mongoose from "mongoose";
-
-const ImageSchema = new mongoose.Schema({
-    images: [{location: {type: String, required: true}}]
-});
-
-export const ImageModel = mongoose.model("Images", ImageSchema);
